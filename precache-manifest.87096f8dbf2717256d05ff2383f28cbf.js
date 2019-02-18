64e154b0b93ea11fb45abc89cf2eb270d36b369b
Louis-Marie MICHELIN <louis-marie.michelin@ensta-paristech.fr>
2019-02-18 23:37:00 +0100

    Updates

---
+++ precache-manifest.87096f8dbf2717256d05ff2383f28cbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f40e7d4b7d5c84d58144",
+    "url": "/theodo-pokemon/static/js/runtime~main.f40e7d4b.js"
+  },
+  {
+    "revision": "1fc34e220c0462a9420b",
+    "url": "/theodo-pokemon/static/js/main.1fc34e22.chunk.js"
+  },
+  {
+    "revision": "a881d5a0072b36cc1620",
+    "url": "/theodo-pokemon/static/js/2.a881d5a0.chunk.js"
+  },
+  {
+    "revision": "1fc34e220c0462a9420b",
+    "url": "/theodo-pokemon/static/css/main.d296d3f9.chunk.css"
+  },
+  {
+    "revision": "f84172e6d8cabca3245c51d85945407b",
+    "url": "/theodo-pokemon/index.html"
+  }
+];
